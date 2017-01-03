e2bc35d65d6a91d39e293ebb4d9e2855b099b630
Jarad Niemi <niemi@iastate.edu>
2017-01-03 08:47:21 -0800

    Add function to update the game to the next player

---
+++ R/next_player.R
@@ -0,0 +1,15 @@
+#' Updates the game state to the next player
+#'
+#' @param game A current game state
+#' @return An updated game
+#' @seealso \link{\code{play}}
+#'
+next_player <- function(game) {
+  n_players <- ncol(game$player)
+  game$turn <- game$turn + 1
+
+  if (game$turn > n_players)
+    game$turn <- 1
+
+  return(game)
+}
