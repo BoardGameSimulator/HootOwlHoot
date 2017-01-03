7c65d533b89fdddc026ca2ddc2a9648593eb2416
Jarad Niemi <niemi@iastate.edu>
2017-01-03 14:16:54 -0800

    Line up <-

--- R/discard_and_draw.R
+++ R/discard_and_draw.R
@@ -11,9 +11,10 @@ discard_and_draw <- function(card, game) {
   player_cards <- game$player[,game$turn]
   player_cards <- player_cards[-match(card, player_cards)]
   player_cards <- c(player_cards, game$deck[1])
+  game$player[,game$turn] <- player_cards
 
   game$discard <- c(game$discard, card)
-  game$deck <- game$deck[-1]
+  game$deck    <- game$deck[-1]
 
   return(game)
 }
